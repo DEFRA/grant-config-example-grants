2567b6f4e447ebcadbdbf3b6a769e7d9d94e8287
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-04-29 14:57:02 +0100

    Update example-grant-with-auth and add acceptance test run

---
+++ .changeset/kind-boxes-marry.md
@@ -0,0 +1,5 @@
+---
+"grant-config-example-grants": patch
+---
+
+Minor change to example-grant-with-auth. plus run acceptance tests when config changes
---
+++ test/run-all-tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Usage:
+#   Run all acceptance tests:
+#     ./tools/run-all-tests.sh
+
+TEST_COMMAND='npm run test:ci'
+
+export COMPOSE_EXPERIMENTAL_GIT_REMOTE=true
+
+export ACCEPTANCE_TESTS_HOOK="
+  docker compose -f https://github.com/DEFRA/grants-ui.git#main:compose.tests.yml run --quiet-pull --rm grants-ui-acceptance-tests $TEST_COMMAND &&
+  docker compose -f https://github.com/DEFRA/grants-ui.git#main:compose.tests.yml down
+"
+mkdir -p test/testconfig
+cp -r example-grant-with-auth/ test/testconfig/example-grant-with-auth@0.0.0
+cp $(dirname "$0")/release.yml test/testconfig/
+
+"$(dirname "$0")/docker-compose-smoke-test.sh"
