dc472af00e4eec230877197a5f824344d1736a69
Stephen Powell <stephen.powell@esynergy.co.uk>
2026-05-27 12:48:41 +0100

    mount example-grant-with-auth submission schema into acceptance tests container

--- test/run-all-tests.sh
+++ test/run-all-tests.sh
@@ -9,9 +9,11 @@ TEST_COMMAND='npm run test:ci'
 
 export COMPOSE_EXPERIMENTAL_GIT_REMOTE=true
 
+OVERRIDE_FILE="$(cd "$(dirname "$0")" && pwd)/compose.acceptance.localconfig.yml"
+
 export ACCEPTANCE_TESTS_HOOK="
-  docker compose -f https://github.com/DEFRA/grants-ui.git#main:compose.tests.yml run --interactive=false -T --quiet-pull --rm grants-ui-acceptance-tests $TEST_COMMAND &&
-  docker compose -f https://github.com/DEFRA/grants-ui.git#main:compose.tests.yml down
+  docker compose -f https://github.com/DEFRA/grants-ui.git#main:compose.tests.yml -f ${OVERRIDE_FILE} run --interactive=false -T --quiet-pull --rm grants-ui-acceptance-tests $TEST_COMMAND &&
+  docker compose -f https://github.com/DEFRA/grants-ui.git#main:compose.tests.yml -f ${OVERRIDE_FILE} down
 "
 
 if [ "${CI}" = "true" ]; then
@@ -21,5 +23,8 @@ fi
 mkdir -p test/testconfig
 cp -r example-grant-with-auth/ test/testconfig/example-grant-with-auth@0.0.0
 cp $(dirname "$0")/release.yml test/testconfig/
+
+mkdir -p test/testconfig/schemas
+cp example-grant-with-auth/grants-ui/example-grant-with-auth-submission.schema.json test/testconfig/schemas/
 
 "$(dirname "$0")/docker-compose-smoke-test.sh"
